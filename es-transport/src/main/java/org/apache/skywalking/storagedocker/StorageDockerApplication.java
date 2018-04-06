0b165981936f28798ee1db43edb0e62fe4394761
28707699@qq.com <8m*ivy00>
2018-04-06 20:24:33 +0800

    create docker build files

---
+++ es-transport/src/main/java/org/apache/skywalking/storagedocker/StorageDockerApplication.java
@@ -0,0 +1,12 @@
+package org.apache.skywalking.storagedocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageDockerApplication.class, args);
+	}
+}
